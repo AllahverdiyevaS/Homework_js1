7f858779f05edc57699617fbcc16f3febbdc34be
AllahverdiyevaS <135016767+AllahverdiyevaS@users.noreply.github.com>
2023-06-21 23:01:40 +0000

    Add files via upload
    Homework js

---
+++ script.js
@@ -0,0 +1,24 @@
+// const valueNum = +prompt("Write a number")
+// console.log(valueNum * 0.1)
+
+// const firstNum = +prompt("Write a first number")
+// const secondNum = +prompt("Write a second number")
+// if (firstNum < secondNum) {console.log(firstNum) }
+// else {console.log(secondNum)}
+
+// const number = +prompt("Write a number")
+// if (number < 100) { console.log("Число меньше 100")}
+// else if (number > 100) { console.log("Число больше 100") }
+// else { console.log("Число равно 100")}
+
+
+const userName = prompt("Write your name");
+const userAge = +prompt("Write your age");
+
+if (userAge > 18) {
+  console.log(`Hello, ${userName}`);
+} else {
+  console.log(`Hi, ${userName}`);
+}
+
+
